8f137c682346dca2c7875d5806bc6b37b00c1a1b
Rinmy <kisimoto.aki@gmail.com>
2021-05-02 03:10:42 +0900

    変数名とかちょっと変更

--- main.go
+++ main.go
@@ -13,8 +13,9 @@ import(
 type Config struct {
 	Port int `json:"port"`
 }
+var config Config
 
-func handler(res http.ResponseWriter, req *http.Request){
+func handler(response http.ResponseWriter, request *http.Request){
 	res.Header().Set("Content-Type", "text/html")
 	res.Header().Set("X-Frame-Options", "DENY")
 	res.Header().Set("X-XSS-Protection", "1; mode=block")
@@ -25,6 +26,8 @@ func handler(res http.ResponseWriter, req *http.Request){
 	const html = `<doctype html>
 <html>
 	<head>
+		<link rel="stylesheet" href="/css/reset.css">
+		<link rel="stylesheet" href="/css/main.css">
 		<script src="/js/title.js"></script>
 	</head>
 	<body>
@@ -34,13 +37,16 @@ func handler(res http.ResponseWriter, req *http.Request){
 	// 最後に出力するコンテンツ
 	var content string
 
+	// 最初のスラッシュ削除しつつスライス？配列？に分割
 	var pathSplit = strings.Split(req.URL.Path[1:], "/")
+
 	switch(len(pathSplit)){
 	case 1:
 		switch(pathSplit[0]){
-		case "settings":
-		case "login":
-		case "logout":
+		case "": fallthrough
+		case "settings": fallthrough
+		case "login": fallthrough
+		case "logout": fallthrough
 		case "signup":
 			status = 200
 			content = html
@@ -68,14 +74,14 @@ func handler(res http.ResponseWriter, req *http.Request){
 	}
 
 	if(content == ""){
-		var resourcePath = fmt.Sprintf("./resources%s", req.URL.Path)
-		var data, error = ioutil.ReadFile(resourcePath)
+		var resourcePath = fmt.Sprintf("./resources%s", request.URL.Path)
+		var bytes, error = ioutil.ReadFile(resourcePath)
 		if(error != nil){
 			content = html
 		}else{
-			var index = strings.LastIndex(req.URL.Path, ".")
+			var index = strings.LastIndex(request.URL.Path, ".")
 			if(index != -1){
-				var extension = req.URL.Path[index:]
+				var extension = request.URL.Path[index:]
 				var mimeType = mime.TypeByExtension(extension)
 				res.Header().Set("Content-Type", mimeType)
 			}else{
@@ -84,30 +90,31 @@ func handler(res http.ResponseWriter, req *http.Request){
 			}
 
 			status = 200
-			content = string(data)
+			content = string(bytes)
 		}
 	}
 
-	res.WriteHeader(status)
+	response.WriteHeader(status)
 	fmt.Fprintf(res, "%s", content)
 }
 
 func main(){
-	var configData, error = ioutil.ReadFile("./config.json")
-	if(error != nil){
-		fmt.Println(error)
+	var bytes, err = ioutil.ReadFile("./config.json")
+	if(err != nil){
+		fmt.Println(err)
+		return
 	}
 
-	var config Config
-	error = json.Unmarshal(configData, &config)
-	if(error != nil){
-		fmt.Println(error)
+	err = json.Unmarshal(bytes, &config)
+	if(err != nil){
+		fmt.Println(err)
+		return
 	}
+
+	var port = ":" + strconv.Itoa(config.Port)
 
 	var mux = http.NewServeMux()
 	mux.HandleFunc("/", handler)
-	fmt.Println("Akitter API Server KIDOU!")
-
-	var port = ":" + strconv.Itoa(config.Port)
+	fmt.Println("Akitter Server KIDOU!")
 	http.ListenAndServe(port, mux)
 }
